2e3a0b1168b867c89e742dd4b6be6e59c910be21
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-05-26 09:46:09 -0300

    Add files via upload

---
+++ atv-zapzap/src/script.js
@@ -0,0 +1,7 @@
+function enviarWhats(){
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let numero = document.getElementById("numero").value;
+
+    
+}
