ea54f769836c60de16d2459bc5528c835cf86199
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-05-26 12:21:10 -0300

    Add files via upload

--- atv-zapzap/src/script.js
+++ atv-zapzap/src/script.js
@@ -1,7 +1,16 @@
 function enviarWhats(){
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
-    let numero = document.getElementById("numero").value;
+    let telefone = document.getElementById("numero").value;
 
-    
+     let mensagem =
+    `Olá!%0A
+Nome: ${nome}%0A
+E-mail: ${email}%0A
+WhatsApp: ${telefone}`;
+
+    let numero = "5511999999999";
+    let url = `https://wa.me/${numero}?text=${mensagem}`;
+    window.open(url,"_blank")
+
 }
